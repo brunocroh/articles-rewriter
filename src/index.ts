cdaa1158489d7fbf125b94822cf945696dad50e4
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-22 23:19:11 +0100

    feat: use all implemented modules to try fetch entire article

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
 import { loadContent } from "./document-loaders/html";
 import { htmlToText } from "./output-parsers//article";
-import { extractArticle, extractTitle } from "./prompt";
+import { extractTitle, isArticleContent } from "./prompt";
 
 const url =
   "https://www.tomshardware.com/pc-components/ssds/new-samsung-ssds-are-allegedly-right-around-the-corner-990-evo-plus-and-9100-pro-trademarks-have-surfaced";
 const init = async () => {
+  const title = await extractTitle(url);
+
   const docs = await loadContent(url);
-  const result = await htmlToText(docs);
+  const chunks = await htmlToText(docs);
 
-  const reducedDocs = result.map((item) => item.pageContent);
+  const articleChunks = [];
 
-  // const title = await extractTitle(JSON.stringify(result));
-  // const article = await extractArticle(JSON.stringify(reducedDocs));
+  for (const [index, chunk] of chunks.entries()) {
+    const response = await isArticleContent(chunk.pageContent);
 
-  console.log({ title: result[0].pageContent.length });
-  console.log({ article: reducedDocs.join(" ").length });
+    console.log(`isContent: ${response.isContent} : ${index}/${chunks.length}`);
+
+    if (response.isContent) {
+      articleChunks.push(chunk);
+      console.log({ chunk });
+    }
+  }
+
+  console.log({ title });
+
+  console.log(articleChunks.map((doc) => doc.pageContent || ""));
 };
 
 init();
