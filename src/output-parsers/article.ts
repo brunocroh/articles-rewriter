18fcf08e26cff77a0a3b9f14673fa3d607aba91c
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-22 23:18:43 +0100

    feat: update output parsers

--- src/output-parsers/article.ts
+++ src/output-parsers/article.ts
@@ -1,9 +1,10 @@
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-import { HtmlToTextTransformer } from "@langchain/community/document_transformers/html_to_text";
+import { MozillaReadabilityTransformer } from "@langchain/community/document_transformers/mozilla_readability";
+// import { HtmlToTextTransformer } from "@langchain/community/document_transformers/html_to_text";
 
 export const htmlToText = async (docs: any) => {
   const splitter = RecursiveCharacterTextSplitter.fromLanguage("html");
-  const transformer = new HtmlToTextTransformer();
+  const transformer = new MozillaReadabilityTransformer();
 
   const sequence = splitter.pipe(transformer);
   const newDocuments = await sequence.invoke(docs);
