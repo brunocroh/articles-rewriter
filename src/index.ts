cd46aa91b7efb29d15f76edbb2b05d0f6065f385
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-24 22:41:04 +0100

    feat: add new prompts

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,60 @@
 import { loadContent } from "./document-loaders/html";
-import { htmlToChunks } from "./output-parsers//article";
-import { extractTitle, sanitizeHTML } from "./prompt";
+import { htmlToChunks, textToChunks } from "./output-parsers//article";
+import {
+  extractArticle,
+  extractTitle,
+  removeUnrelatedContent,
+  rewrite,
+  translate,
+} from "./prompt";
 
 const url =
-  "https://www.tomshardware.com/pc-components/ssds/new-samsung-ssds-are-allegedly-right-around-the-corner-990-evo-plus-and-9100-pro-trademarks-have-surfaced";
+  "https://www.tomshardware.com/raspberry-pi/this-custom-raspberry-pi-pc-is-battery-powered-has-a-built-in-handle-and-glows-with-rgb-leds";
+
 const init = async () => {
   const article = await loadContent(url);
 
   if (!article?.content) {
     return;
   }
+
   const docs = await htmlToChunks(article.content);
 
-  let newArticle = "";
+  console.log({ chunks: docs.length });
+
+  let draftArticle = "";
 
   for (const [index, chunk] of docs.entries()) {
-    const content = await sanitizeHTML(chunk.pageContent);
+    const content = await extractArticle(chunk);
 
     if (content) {
-      newArticle += content;
+      draftArticle += `${content} \n\n`;
     }
 
-    console.log(`${index}/${docs.length}`);
+    console.log(`${index + 1}/${docs.length}`);
   }
 
-  console.log({ newArticle });
+  const pipeline = [
+    removeUnrelatedContent,
+    rewrite,
+    async (text: string) => translate(text, "Brazilian portuguese"),
+  ];
+
+  const finalArticle = await pipeline.reduce(
+    async (content: Promise<string> | string, fn) => {
+      if (typeof content !== "string") {
+        const _content = await content;
+        console.log(_content);
+        return fn(await content);
+      }
+      return fn(content);
+    },
+    draftArticle,
+  );
+
+  console.log(finalArticle);
+
+  console.log({ length: finalArticle.length });
 };
 
 init();
