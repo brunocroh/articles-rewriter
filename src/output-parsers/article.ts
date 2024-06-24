3270930611d0b9aeaf9e969fb196c378c2a02434
Bruno Rodrigues <brunocroh@gmail.com>
2024-06-24 22:40:51 +0100

    feat: improve project entrypoint

--- src/output-parsers/article.ts
+++ src/output-parsers/article.ts
@@ -1,9 +1,21 @@
-import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import {
+  RecursiveCharacterTextSplitter,
+  CharacterTextSplitter,
+} from "@langchain/textsplitters";
 
-export const htmlToChunks = async (htmlText: string) => {
+export const htmlToChunks = async (text: string) => {
   const htmlSplitter = RecursiveCharacterTextSplitter.fromLanguage("html", {
     chunkSize: 1000,
     chunkOverlap: 0,
   });
-  return await htmlSplitter.createDocuments([htmlText]);
+  return await htmlSplitter.splitText(text);
+};
+
+export const textToChunks = async (text: string) => {
+  const htmlSplitter = new CharacterTextSplitter({
+    separator: "\n",
+    chunkSize: 200,
+    chunkOverlap: 0,
+  });
+  return await htmlSplitter.splitText(text);
 };
